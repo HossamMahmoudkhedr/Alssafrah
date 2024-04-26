061c23669b85279900e267938456dabdb466e7ab
Yousef Abdo <94499619+yousefabdo200@users.noreply.github.com>
2024-04-26 18:51:50 +0300

    add databse

--- database/sql.sql
+++ database/sql.sql
@@ -1,12 +1,22 @@
 create database Alssafarah ;
 use Alssafarah;
+create table admins
+(
+	id int primary key auto_increment,
+	name varchar(150) not null,
+	email varchar(250) not null unique,
+	password varchar(500) not null
+);
 create table teachers
 (
 	id int primary key auto_increment,
 	name varchar(150) not null,
 	email varchar(250) not null unique,
 	phone varchar(50),
-	password varchar(500) not null
+	password varchar(500) not null,
+	Alhalka_Number  varchar(5) not null unique,
+    admin_id int not null ,
+    foreign key (admin_id) references admins(id)
 );
 
 create table parents
@@ -15,33 +25,32 @@ create table parents
 	name varchar(150) not null,
 	email varchar(250) ,
 	phone varchar(50) not null unique,
-	password varchar(500) not null
+	password varchar(500) not null,
+	admin_id int not null ,
+    foreign key (admin_id) references admins(id)
 );
 create table students
 (
 	id int primary key auto_increment,
 	name varchar(150) not null,
-	phone varchar(50) not null,
-    parent_id int not null,
-    foreign key (parent_id) references parents(id) 
-);
-
-create table teachers_stuents_evaluations
-(
-	teacher_id int not null,
-    student_id int not null,
-    new_sura_start_name varchar(50) not null,
-    new_sura_start_number varchar(5) not null,
-	news_ura_end_name varchar(50) not null,
-	new_sura_end_number varchar(5) not null,
-    previous_sura_star_name varchar(50) not null,
-    previous_sura_start_number varchar(5) not null,
-    previous_sura_end_name varchar(50) not null,
-    previous_sura_end_number varchar(5) not null,
+    ssn varchar(50) not null unique,
+	parent_phone varchar(50) not null,
+	new_sura_start_name varchar(50) ,
+    new_sura_start_number varchar(5) ,
+	new_sura_end_name varchar(50) ,
+	new_sura_end_number varchar(5) ,
+    revision_sura_star_name varchar(50),
+    revision_sura_start_number varchar(5) ,
+    revision_sura_end_name varchar(50) ,
+    revision_sura_end_number varchar(5),
     behavior varchar(250),
     attend bool default false,
+    parent_id int not null,
+	admin_id int not null ,
+    teacher_id int not null,
 	foreign key (teacher_id) references teachers(id) ,
-	foreign key (student_id) references students(id) 
+    foreign key (admin_id) references admins(id),
+    foreign key (parent_id) references parents(id) 
 );
 
 
