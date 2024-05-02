7c4c2ca02a06e3a71990f9963ee31f9df03e8f1b
HossamMahmoudKhedr <nhem318@gmail.com>
2024-05-02 23:51:31 +0300

    modify admin login

--- js/adminHandleLogin.js
+++ js/adminHandleLogin.js
@@ -21,9 +21,6 @@ form.addEventListener('submit', (e) => {
 		body: formData,
 	})
 		.then((response) => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
 			return response.json();
 		})
 		.then((data) => {
