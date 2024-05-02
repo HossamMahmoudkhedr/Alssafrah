9399aec1c72fd468a1cbecc2e033d062d577c557
HossamMahmoudKhedr <nhem318@gmail.com>
2024-05-02 15:00:39 +0300

    fix the post problem

--- js/formHandling.js
+++ js/formHandling.js
@@ -1,21 +1,26 @@
 const form = document.querySelector('form');
-const input = document.querySelectorAll('input');
+const input = document.querySelector('input');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
 const formData = new FormData();
 
-input.forEach((el) => {
-	el.onchange = () => {
-		formData.append(el.name, el.value);
-	};
-});
+formData.append('type', 'admin');
+
+email.onchange = () => {
+	formData.append(email.name, email.value);
+	console.log(formData);
+};
+password.onchange = () => {
+	formData.append(password.name, password.value);
+	console.log(formData);
+};
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	fetch('http://localhost/php/Alssafrah/api/admin/addteacher.php', {
+
+	fetch('http://localhost/php/Alssafrah/api/admin/adminlogin.php', {
 		method: 'POST',
 		body: formData,
-		headers: {
-			'Content-Type': 'application/json',
-		},
 	})
 		.then((response) => {
 			if (!response.ok) {
